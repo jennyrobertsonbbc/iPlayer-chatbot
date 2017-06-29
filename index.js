99891fc377f43774bb525704ab558ff4ff819a89
Jenny Robertson <jenny.robertson@bbc.co.uk>
2017-06-29 14:22:51 +0100

    Added peter kay response

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ $(document).ready(function(){
 
   function processInput (message) {
       displayUserMessage(message);
-      chooseReply(message);
+      const reply = chooseReply(message);
+      displayBotMessage(reply);
   }
 
   function displayUserMessage(message) {
@@ -38,13 +39,56 @@ $(document).ready(function(){
     },800);
   }
 
-  function chooseReply(message) {
-    if(message.includes('watch')){
-      displayBotMessage('What mood are you in today?');
+  function chooseReply(rawMessage) {
+    const message = rawMessage.toLowerCase();
+
+    if (message.includes('watch')) {
+      return 'What mood are you in today?';
     }
-    if(message.includes('happy')){
-      displayBotMessage("Okay, here's some shows which match your happy mood");
+    if (message.includes('happy')) {
+      const programmeName = "Peter Kay's Comedy Shuffle ";
+      const programmeLink = "http://www.bbc.co.uk/iplayer/episode/b08w8gfq/peter-kays-comedy-shuffle-series-2-episode-1";
+      return `Okay, you might enjoy \n<a href='${programmeLink}'>${programmeName} <img class='programme-image' src='img/p03qq9lt.jpg'></a>`;
     }
+    if (message.includes('thanks') || message.includes('thank you') || message.includes('thankyou')){
+      return 'No, thank you :D';
+    }
+
+    return "I'm sorry I don't know how to answer that.";
   }
 });
+ //when is eastenders next on
+ //whats on bbc 1 right now - link to live
+ //how old are you? ?
 
+ const responses = {
+
+  trigger: "watch",
+  response: "What mood are you in today?",
+  children: [
+    {
+      trigger: "happy",
+      message: "Here's a happy show you might enjoy"
+    },
+    {
+      trigger: "sad",
+      message: "Would you like to be cheered up?",
+      children: [
+        {
+          trigger: "yes",
+          message: "Here's a happy show for you to watch."
+        },
+        {
+          trigger: "no",
+          message: "Here's a sad for you to watch. Feel better soon!"
+        }
+      ]
+    },
+    {
+      trigger: "angry",
+      message: "Here's a happy show you might enjoy."
+    },
+
+  ]
+
+ };
